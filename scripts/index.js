cb70f083583bee3553972c305f95b07a608a85b8
Maksim Dav <maksim.dav@mail.ru>
2022-02-05 22:00:22 +0800

    JS add six cards and create popup new place

--- scripts/index.js
+++ scripts/index.js
@@ -3,23 +3,78 @@ let profile = document.querySelector('.profile');
 let profileUserName = profile.querySelector('.profile__user-name');
 let profileUserInfo = profile.querySelector('.profile__user-info');
 let profileButtonEdit = profile.querySelector('.profile__button-edit');
+let profileButtonAdd = profile.querySelector('.profile__button-add');
 
 /* Переменные для элемента popup */
 let popup = document.querySelector('.popup');
+let popupEdit = document.querySelector('.popup_role_edit');
+let popupCreate = document.querySelector('.popup_role_create');
 let popupForm = popup.querySelector('.form');
 let popupUserName = popupForm.querySelector('.form__input_role_user-name');
 let popupUserInfo = popupForm.querySelector('.form__input_role_user-info');
-let popupButtonClose = popup.querySelector('.popup__button-close');
+let popupButtonSave = popupForm.querySelector('.form__button-save');
+let popupButtonCreate = popupForm.querySelector('.form__button-create');
+let popupEditButtonClose = popupEdit.querySelector('.popup__button-close');
+let popupCreateButtonClose = popupCreate.querySelector('.popup__button-close');
+
+const cardTemplate = document.querySelector('#card').content;
+const cards = document.querySelector('.cards');
+const initialCards = [
+  {
+    name: 'Альгарробо',
+    link: './images/Алгаллробо.jpg',
+    alt: 'Деревянный мост в городе Альгарробо в Чили'
+  },
+  {
+    name: 'Верона',
+    link: './images/Верона.jpg',
+    alt: 'Лес в провинции Верона в Италии'
+  },
+  {
+    name: 'Амстердам',
+    link: './images/Амстердам.jpg',
+    alt: 'Один из каналов в Амстердаме'
+  },
+  {
+    name: 'Бад-Пирмонт',
+    link: './images/Бад-Пирмонт.jpg',
+    alt: 'Лес в окрестностях города Бад-Пирмонт в Германии'
+  },
+  {
+    name: 'Маттерхорн',
+    link: './images/Маттерхорн.jpg',
+    alt: 'Вид на гору Маттерхорн в Швейцарии'
+  },
+  {
+    name: 'Домбай',
+    link: './images/Домбай.png',
+    alt: 'Домбай'
+  }
+];
+
+function render() {
+  initialCards.forEach(createNewCard);
+}
+
+render();
 
 /* Функции открытия и закрытия popup */
-function popupOpen() {
-  popup.classList.add('popup_opened');
+function popupEditOpen() {
+  popupEdit.classList.add('popup_opened');
   popupUserName.value = profileUserName.textContent;
   popupUserInfo.value = profileUserInfo.textContent;
 }
 
-function popupClose() {
-  popup.classList.remove('popup_opened');
+function popupCreateOpen() {
+  popupCreate.classList.add('popup_opened');
+}
+
+function popupEditClose() {
+  popupEdit.classList.remove('popup_opened');
+}
+
+function popupCreateClose() {
+  popupCreate.classList.remove('popup_opened');
 }
 
 /* Функция перехватывает отправку формы и сохраняет данные из полей ввода */
@@ -27,12 +82,36 @@ function formSubmitHandler(evt) {
   evt.preventDefault();
   profileUserName.textContent = popupUserName.value;
   profileUserInfo.textContent = popupUserInfo.value;
-  popupClose();
+  popupEditClose();
+  popupCreateClose();
 }
 
 /* Отслеживаем нажатие на соотв. кнопки */
-profileButtonEdit.addEventListener('click', popupOpen);
-popupButtonClose.addEventListener('click', popupClose);
+profileButtonEdit.addEventListener('click', popupEditOpen);
+profileButtonAdd.addEventListener('click', popupCreateOpen);
+popupEditButtonClose.addEventListener('click', popupEditClose);
+popupCreateButtonClose.addEventListener('click', popupCreateClose);
 
 /* Отслеживаем событие отправки формы */
 popupForm.addEventListener('submit', formSubmitHandler);
+
+function createNewCard(initialCards) {
+  const newCard = cardTemplate.cloneNode(true);
+  newCard.querySelector('.card__title').textContent = initialCards.name;
+  newCard.querySelector('.card__image').src = initialCards.link;
+  newCard.querySelector('.card__image').alt = initialCards.alt;
+  cards.append(newCard);
+}
+
+function handlerAdd() {
+  
+}
+
+function handlerEdit() {
+  
+}
+
+function addListeners(el) {
+  //el.querySelector('.delete').addEventListener('click', handlerDelete);
+}
+
