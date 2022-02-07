8e42ffc060536a7476c6c5416620f384c175c70b
Maksim Dav <maksim.dav@mail.ru>
2022-02-07 21:15:50 +0800

    move initialCards to the cards.js folder

--- scripts/index.js
+++ scripts/index.js
@@ -33,39 +33,6 @@ const cards = document.querySelector('.cards');
 /* card-template */
 const cardTemplate = document.querySelector('.template-card').content;
 
-const initialCards = [
-  {
-    name: 'Альгарробо',
-    link: './images/Алгаллробо.jpg',
-    alt: 'Деревянный мост в городе Альгарробо в Чили'
-  },
-  {
-    name: 'Верона',
-    link: './images/Верона.jpg',
-    alt: 'Лес в провинции Верона в Италии'
-  },
-  {
-    name: 'Амстердам',
-    link: './images/Амстердам.jpg',
-    alt: 'Один из каналов в Амстердаме'
-  },
-  {
-    name: 'Бад-Пирмонт',
-    link: './images/Бад-Пирмонт.jpg',
-    alt: 'Лес в окрестностях города Бад-Пирмонт в Германии'
-  },
-  {
-    name: 'Маттерхорн',
-    link: './images/Маттерхорн.jpg',
-    alt: 'Вид на гору Маттерхорн в Швейцарии'
-  },
-  {
-    name: 'Домбай',
-    link: './images/Домбай.png',
-    alt: 'Домбай'
-  }
-];
-
 function render(el) {
   el.forEach((item) => {
     renderCard(item);
