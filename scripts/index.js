cb473ec71cbe4e7f6cb53616b2f68397af1e93c5
Maksim Dav <maksim.dav@mail.ru>
2022-02-17 21:43:55 +0800

    add validation on js variant_1

--- scripts/index.js
+++ scripts/index.js
@@ -137,3 +137,72 @@ popupCreateButtonClose.addEventListener('click', () => closePopup(popupCreate));
 popupDisplayButtonClose.addEventListener('click', () => closePopup(popupDisplay));
 popupEditForm.addEventListener('submit', handleEditFormSubmit);
 popupCreateForm.addEventListener('submit', handleCreateFormSubmit);
+
+/* ======================================= */
+
+/* включение валидации страницы */
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.form')); // выбираем все элементы .form на странице
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement); // устанавливаем слушатель событий на каждое поле ввода на выбранной форме
+  });
+};
+
+/* проверка валидности полей ввода и изменение статуса кнопки отправки формы */
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.form__input')); // выбираем все поля ввода на переданной в функцию форме
+  const buttonElement = formElement.querySelector('.form__submit'); // выбираем элемент отправки формы на переданной в функцию форме
+  toggleButtonState(inputList, buttonElement); // если изначально не валидно - меняем статус кнопки отправки формы на неактивную
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {  // вешаем слушатель событий на каждое поле ввода на форме
+      checkInputValidity(formElement, inputElement);  // проверяем валидность после ввода каждого символа
+      toggleButtonState(inputList, buttonElement);  // если не валидно - меняем статус кнопки отправки формы на неактивную
+    });
+  });
+};
+
+/* проверка валидности вводимых данных */
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+/* вывод сообщения об ошибке ввода плюс красная линия под полем ввода */
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('form__input_type_error') // подчёркиваем красным поле ввода
+  errorElement.textContent = errorMessage; // текст сообщения об ошибке ввода
+  errorElement.classList.add('form__input-error_active'); // показываем сообщение об ошибке ввода
+};
+
+/* скрываем сообщение об ошибке и убираем красную линию под полем ввода */
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('form__input_type_error'); // убираем красное подчёркивание поля ввода
+  errorElement.classList.remove('form__input-error_active'); // убираем сообщение об ошибке ввода
+  errorElement.textContent = ''; // очищаем текст сообщения об ошибке ввода
+};
+
+/* меняем статус кнопки отправки формы (активная - неактивная) */
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('button_inactive');
+  } else {
+    buttonElement.classList.remove('button_inactive');
+  }
+};
+
+/* возвращает значение свойства valid первого невалидного элемента */
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+enableValidation();
